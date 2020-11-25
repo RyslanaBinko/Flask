3334245e402407d9289dc953f06dfdc26879bb77
Binko Ruslana <binko.ryslana@gmail.com>
2020-11-25 12:50:49 +0200

    Homework modified according to comments

--- homework_2.py
+++ homework_2.py
@@ -3,39 +3,50 @@ from cryptography.fernet import Fernet
 
 
 app = Flask(__name__)
-key = Fernet.generate_key()
-f = Fernet(key)
+generate_key = Fernet.generate_key()
+key = Fernet(generate_key)
 
 
 @app.route("/")
 @app.route("/encrypt")
-def encrypt(string="string"):
+def encrypt():
 
     '''
 
-    :param string: string for encrypt
-    :return: encrypted string
+    Encrypts the received text
+
+    :param string: string for encrypt:str
+    :return: encrypted result:bytes
+
     '''
 
-    string = request.args["string"]
-    by = string.encode('utf-8')
-    return render_template("encrypt_and_decrypt.html", perem=f.encrypt(by), frase="Encrypted result: ")
+    text = request.args.get("string")
+    if text:
+        convert = text.encode('utf-8')
+        frase = "Encrypted result: "
+        result = key.encrypt(convert)
+    else:
+        frase = "Enter your text to the url"
+        result = ""
+    return render_template("encrypt_and_decrypt.html", result=result, frase=frase)
 
 
 @app.route("/decrypt")
-def decrypt(string="string"):
+def decrypt():
 
     '''
 
-    :param string: string for decrypt
-    :return: decrypted string
+    Deccrypts the received text
+
+    :param string: string for decrypt:str
+    :return: decrypted result:str
 
     '''
 
-    string = request.args["string"]
-    byt = bytes(string, encoding='utf8')
-    dec = f.decrypt(byt)
-    return render_template('encrypt_and_decrypt.html', perem=dec.decode('utf-8'), frase="Decrypted result:")
+    string = request.args.get("string")
+    convert_to_bytes = bytes(string, encoding='utf8')
+    result = key.decrypt(convert_to_bytes)
+    return render_template('encrypt_and_decrypt.html', result=result.decode('utf-8'), frase="Decrypted result:")
 
 
 app.run(debug=True)
